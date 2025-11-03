7cac114a2fe921a3c1bc2ba13d662196ff52dcee
Jonathan Salinas <solojulian01@gmail.com>
2025-11-03 16:00:57 -0300

    primer commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <title>PestControl</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('images/logo-pest control.png') }}" alt="Bootstrap" width="150" height="150">
+                </a>
+            </div>
+            
+        </nav>
+    </header>
+</body>
+</html>
